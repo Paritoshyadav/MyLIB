8534b45ea9d0a5afb4716f01792c5cd61273043a
Paritosh yadav <y.paritosh@gmail.com>
2020-01-18 02:52:40 +0530

    books delete/edit function

--- routes/books.js
+++ routes/books.js
@@ -54,13 +54,13 @@ router.post('/',  async (req,res)=>{
     })
     saveCover(book,req.body.cover)
     
-    console.log(await Book.find({}));
+    
     
     
     
     try{
         const newBook = await book.save()
-        res.redirect('books')
+        res.redirect(`books/${newBook.id}`)
         
         
     }catch{
@@ -74,7 +74,142 @@ router.post('/',  async (req,res)=>{
         
     })
 
+
+
+
+
+
+    /////////////////////ID//////////////////
+
+    router.get('/:id',async (req,res)=>{
+        try{
+          const book = await Book.findById(req.params.id).populate('author').exec()
+          res.render('books/show',{book:book})
+
+        }catch{
+            res.render('/')
+
+        }
+    })
+
+
+    router.get('/:id/edit', async (req,res)=>{
+        try{
+            const book = await Book.findById(req.params.id)
+            
+            renderEditpage(res,book)
+            
+            
+        }catch{
+            res.redirect('/')
+
+        }
+        
+        
+        
+    })
+
+
+    router.put('/:id',  async (req,res)=>{
+       let book
+
+       try{
+           book = await Book.findById(req.params.id)
+           book.title = req.body.title
+           book.author = req.body.author
+           book.publishDate = new Date(req.body.publishDate)
+           book.pageCount = req.body.pageCount
+           book.description = req.body.description
+           if(req.body.cover != null && req.body.cover !== ''){
+               saveCover(book,req.body.cover)
+           }
+           await book.save()
+           res.redirect(`books/${newBook.id}`)
+
+       }catch{
+           if (book !=null){
+               renderEditpage(res,book,true)
+           } else{
+               res.redirect('/')
+           }
+
+       }
+    })
+    
+
+    router.delete('/:id', async (req,res)=>{
+        let book
+        try{
+            book = await Book.findById(req.params.id)
+            await book.remove()
+            res.redirect('/books')
+
+        }catch(err){
+            console.log(err);
+            
+            if(book != null){
+                res.render('books/show',{
+                    book:book,
+                    errormsg:'could not remove book'
+                })
+            }else{
+                res.redirect('/')
+            }
+
+        }
+
+
+    })
+
+    
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /////////////functions/////////////////////
+    
     async function renderNewpage(res,book,hasError=false){
+        renderFormpage(res,book,'new',hasError)
+
+    }
+
+    function saveCover(book,encodedCover){
+        
+        try{
+            if(encodedCover ==  null) return
+        const cover =  JSON.parse(encodedCover)
+        
+        if(cover != null && imageMimeTypes.includes(cover.type)){
+            book.CoverImage = new Buffer.from(cover.data,'base64')
+            book.coverImagetype = cover.type
+        }
+
+    }catch{
+        console.log("Error creating book");
+        
+
+
+        }
+
+        }
+        
+    async function renderEditpage(res,book,form,hasError=false){
+        renderFormpage(res,book,'edit',hasError)
+
+    }
+
+
+    async function renderFormpage(res,book,form,hasError=false){
         try{
             const authors = await Author.find({})
         
@@ -82,25 +217,25 @@ router.post('/',  async (req,res)=>{
             authors:authors,
             book:book
         }
-        if (hasError) params.errormsg ="Error Creating book"
-        res.render('books/new',params)
+        if (hasError){
+            if(form === 'edit'){
+                params.errormsg ="Error Updating book" 
+            }else{
+                params.errormsg ="Error Creating book"
+
+            }
+        }
+        res.render(`books/${form}`,params)
+        
     }
         
         catch{
-            res.redirect('/books')
+            res.redirect('/')
             
         }
     }
 
-    function saveCover(book,encodedCover){
-        if(encodedCover ==  null) return
-        const cover =  JSON.parse(encodedCover)
-        if(cover != null && imageMimeTypes.includes(cover.type)){
-            book.CoverImage = new Buffer.from(cover.data,'base64')
-            book.coverImagetype = cover.type
-        }
 
-    }
 
     // function removeBookCover(fileName){
     //     fs.unlink(path.join(uploadPath,fileName),err=>{
