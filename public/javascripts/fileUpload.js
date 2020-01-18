db811cc351e5de5bad3c044bc7d31adbdb7fcf87
Paritosh yadav <y.paritosh@gmail.com>
2020-01-19 01:24:32 +0530

    Final Touch

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,8 +1,22 @@
-FilePond.registerPlugin(
+const rootStyles = window.getComputedStyle(document.documentElement)
+
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') !== '') {
+  ready()
+} else {
+  document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready() {
+  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+  const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+  const coverHeight = coverWidth / coverAspectRatio
+  FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
-    FilePondPluginFileEncode
-)
+    FilePondPluginFileEncode,
+  )
+
+
 
 FilePond.setOptions({
     stylePanelAspectRatio: 150 / 100,
@@ -12,3 +26,4 @@ FilePond.setOptions({
 
 
 FilePond.parse(document.body);
+}
